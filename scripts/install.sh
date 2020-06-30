478e7bd8914fa2b6b11ccccf5bed44f9f854da1d
Andrew Deng <pengfei.deng@nttdata.com>
2020-06-30 11:35:06 +0800

    init

---
+++ scripts/install.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+# remove comment if you want to enable debugging
+#set -x
+
+if [ -e /etc/redhat-release ] ; then
+  REDHAT_BASED=true
+fi
+
+TERRAFORM_VERSION="0.12.18"
+PACKER_VERSION="1.2.4"
+# create new ssh key
+[[ ! -f /home/ubuntu/.ssh/mykey ]] \
+&& mkdir -p /home/ubuntu/.ssh \
+&& ssh-keygen -f /home/ubuntu/.ssh/mykey -N '' \
+&& chown -R ubuntu:ubuntu /home/ubuntu/.ssh
+
+# install packages
+if [ ${REDHAT_BASED} ] ; then
+  yum -y update
+  yum install -y docker ansible unzip wget
+else 
+  apt-get update
+  apt-get -y install docker.io ansible unzip python3-pip
+fi
+# add docker privileges
+usermod -G docker ubuntu
+# install awscli and ebcli
+pip3 install -U awscli
+pip3 install -U awsebcli
+
+#terraform
+T_VERSION=$(/usr/local/bin/terraform -v | head -1 | cut -d ' ' -f 2 | tail -c +2)
+T_RETVAL=${PIPESTATUS[0]}
+
+[[ $T_VERSION != $TERRAFORM_VERSION ]] || [[ $T_RETVAL != 0 ]] \
+&& wget -q https://releases.hashicorp.com/terraform/${TERRAFORM_VERSION}/terraform_${TERRAFORM_VERSION}_linux_amd64.zip \
+&& unzip -o terraform_${TERRAFORM_VERSION}_linux_amd64.zip -d /usr/local/bin \
+&& rm terraform_${TERRAFORM_VERSION}_linux_amd64.zip
+
+# packer
+P_VERSION=$(/usr/local/bin/packer -v)
+P_RETVAL=$?
+
+[[ $P_VERSION != $PACKER_VERSION ]] || [[ $P_RETVAL != 1 ]] \
+&& wget -q https://releases.hashicorp.com/packer/${PACKER_VERSION}/packer_${PACKER_VERSION}_linux_amd64.zip \
+&& unzip -o packer_${PACKER_VERSION}_linux_amd64.zip -d /usr/local/bin \
+&& rm packer_${PACKER_VERSION}_linux_amd64.zip
+
+# clean up
+if [ ! ${REDHAT_BASED} ] ; then
+  apt-get clean
+fi
